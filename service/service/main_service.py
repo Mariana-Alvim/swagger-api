39673f7381e9c7f94a904e44b465fddebc035c4f
Mariana Resende Alvim <104532249+Mariana-Alvim@users.noreply.github.com>
2022-06-20 15:36:41 -0300

    Add files via upload

---
+++ service/service/main_service.py
@@ -0,0 +1,44 @@
+import time
+import json
+from loguru import logger
+from service.constants import mensagens
+import pandas as pd
+import numpy as np
+import requests
+from pycep_correios import get_address_from_cep, WebService
+from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+import datetime
+from datetime import datetime
+
+
+class InfoDataHora:
+    def __init__(self):
+        logger.debug(mensagens.INICIO_LOAD_SERVICO)
+
+
+    def executar_rest(self, cep):
+       
+        logger.debug(mensagens.INICIO_BUSCA)
+        start_time = time.time()
+
+
+        response_local = self.get_address_from_cep(cep, webservice=WebService.APICEP)
+        adress = (response_local['logradouro'] + ' ' + response_local['cidade'] + ' ' + response_local['uf'])
+
+        
+        geolocaliza = Nominatim(user_agent="geoapi")
+        coords = geolocaliza.geocode(adress)
+        longitude = (coords.longitude)
+        latitude = (coords.latitude) 
+
+
+        tf = TimezoneFinder()
+        timezone = tf.timezone_at(lng=longitude,lat=latitude)
+
+        
+        url_timezone = 'http://worldtimeapi.org/api/timezone/' + timezone
+        data_hora_formatar = requests.get(url_timezone)
+        data_hora = datetime.fromisoformat(data_hora_formatar.json()['datetime'])
+                
+        return data_hora
